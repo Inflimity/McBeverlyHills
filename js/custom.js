6b7b8ed1d9710def6d437899b1a069ca0fc66395
inflimity <bamideleminerva@gmail.com>
2026-01-04 15:30:00 +0100

    Adding cookies permission

--- js/custom.js
+++ js/custom.js
@@ -1,33 +1,47 @@
 
-  (function ($) {
-  
+(function ($) {
+
   "use strict";
 
-    // COUNTER NUMBERS
-    jQuery('.counter-thumb').appear(function() {
-      jQuery('.counter-number').countTo();
-    });
-    
-    // CUSTOM LINK
-    $('.smoothscroll').click(function(){
+  // COUNTER NUMBERS
+  jQuery('.counter-thumb').appear(function () {
+    jQuery('.counter-number').countTo();
+  });
+
+  // CUSTOM LINK
+  $('.smoothscroll').click(function () {
     var el = $(this).attr('href');
     var elWrapped = $(el);
     var header_height = $('.navbar').height();
 
-    scrollToDiv(elWrapped,header_height);
+    scrollToDiv(elWrapped, header_height);
     return false;
 
-    function scrollToDiv(element,navheight){
+    function scrollToDiv(element, navheight) {
       var offset = element.offset();
       var offsetTop = offset.top;
-      var totalScroll = offsetTop-navheight;
+      var totalScroll = offsetTop - navheight;
 
       $('body,html').animate({
-      scrollTop: totalScroll
+        scrollTop: totalScroll
       }, 300);
     }
-});
-    
-  })(window.jQuery);
+  });
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const banner = document.getElementById("cookie-banner");
+    const btn = document.getElementById("accept-cookies-btn");
+
+    if (!localStorage.getItem("cookieConsent")) {
+      setTimeout(() => banner.classList.add("active"), 1000); // 1 second delay
+    }
+
+    btn.addEventListener("click", () => {
+      localStorage.setItem("cookieConsent", "true");
+      banner.classList.remove("active");
+    });
+  });
+
+})(window.jQuery);
 
 
